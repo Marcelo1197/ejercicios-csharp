26a52bbe1a74268e54e74eaa06179544dfba8efe
Marcelo1197 <sanchez.marcelo2010@gmail.com>
2022-08-10 21:24:29 -0300

    OK & FIX: archivo renombrado y nuevo ejercicio

---
+++ EjerciciosCsharp/Sesion1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjerciciosCsharp
+{
+    internal class Sesion1
+    {
+        private static void exMain(string[] args)
+        {
+            //Ejercicio 1 - Lee datos de una persona y devuelve en consola
+            Console.WriteLine("Escribe tu nombre:");
+            string nombre = Console.ReadLine();
+            Console.WriteLine("Escribe tu edad");
+            byte edad = Convert.ToByte(Console.ReadLine()); //byte porque nadie podría tener -0 ni +255 años
+
+            Console.WriteLine("Tu nombre es " + char.ToUpper(nombre[0]) + nombre.Substring(1) + " y tienes " + edad + " años");
+
+            Console.WriteLine("\n\n Ahora ingresa la hora en el formato hora:minutos");
+            string hora = Console.ReadLine();
+            Console.WriteLine("La hora ingresada es: " + hora + " hs,");
+        }
+    }
+}
---
+++ EjerciciosCsharp/Sesion2.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjerciciosCsharp
+{
+    internal class Sesion2
+    {
+        static void Main(string[] args)
+        {
+            //Ejercicio 1 - Lee datos de una persona y devuelve en consola
+            Console.WriteLine("Escribe tu nombre:");
+            string nombre = Console.ReadLine();
+            Console.WriteLine("Escribe tu edad:");
+            int edad = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("¿Sabes programar? Ingresa SI o NO");
+            Boolean sabeProgramar = ((Console.ReadLine().ToLower()) == "si") ? true : false;
+            Console.WriteLine($"Tu nombre es {nombre} tienes {edad} años y {((sabeProgramar == true) ? "si sabes programar" : "no sabes programar")}");
+
+            //Ejercicio 2 - Descripcion de objetos
+            //Coche
+            int puertas = 5;
+            int ruedas = 4;
+            string marca = "nissan";
+            Boolean itvVigente = false;
+            //Mesa
+            double peso = 5.5;
+            double largo = 2.5;
+            string material = "pino";
+            string color = "negra";
+
+            //Ejercicio 3
+            Boolean mayorIgual18 = 22 > 18;
+            Boolean charIgual = 'B' == 'a';
+            Boolean ambasCondicionesTrue = 22.GetType() == typeof(int) && 22 > 10;
+            Boolean unaCondicionTrue = 22.GetType() == typeof(int) || 22 > 99999;
+
+        }
+    }
+}
